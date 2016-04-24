e46266f997358472122fe9465708c973a3a11af4
straysheep3 <getloose.kage@gmail.com>
2016-04-24 17:30:44 +0900

    掲載開始日が掲載終了日より前になるようテストで確認

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ActiveRecord::Base
+  validates :title, :body, :released_at, presence: true
+  validates :title, length: { maximum: 200 }
+  validate :check_expired_at
+
+  private
+  def check_expired_at
+    if expired_at && expired_at < released_at
+      errors.add(:expired_at, :expired_at_too_old)
+    end
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -9,4 +9,19 @@ class ArticleTest < ActiveSupport::TestCase
     assert article.errors.include?(:body)
     assert article.errors.include?(:released_at)
   end
+  # 長さのチェック
+  test "length" do
+    article = FactoryGirl.build(:article)
+    article.title = "A" * 201
+    assert article.invalid?
+    assert article.errors.include?(:title)
+  end
+  # 掲載終了日時は掲載開始日時より後
+  test "expired_at" do
+    article = FactoryGirl.build(:article)
+    article.released_at = Time.current
+    article.expired_at = 1.days.ago
+    assert article.invalid?
+    assert article.errors.include?(:expired_at)
+  end
 end
