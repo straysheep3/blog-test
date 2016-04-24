911ea21e6120a52df7a58b09ac657eb63d0a51d5
straysheep3 <getloose.kage@gmail.com>
2016-04-24 10:37:52 +0900

    membersにvalidatesを作成

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,27 @@
 class Member < ActiveRecord::Base
-  validates :number, presence: true
+  include EmailAddressChecker
+  
+  validates :number, presence: true,
+    numericality: { only_integer: true,
+      greater_than: 0, less_than: 100, allow_blank: ture },
+    uniqueness: true
+
+  validates :name, presence: ture,
+    format: { with: /\A[A-Za-z]\w*\z/, allow_blank: true },
+    length: { minimum: 2, maximum: 20, allow_blank: true },
+    uniqueness: { case_sensitive: false }
+
+  validates :full_name, length: { maximum: 20}
+
+  validate :check_email
+
+  private
+  def method_name
+    if email.present?
+      errors.add(:email, :invalid) unless well_formed_as_email_address(email)
+    end
+  end
+
   class << self
     def search(query)
       rel = order("number")
